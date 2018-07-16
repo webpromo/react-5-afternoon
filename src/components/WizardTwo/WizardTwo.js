339f531d9a784bc0e6231f9455496b6be81f99d4
Jesse <rank_booster@yahoo.com>
2018-07-16 13:14:58 -0600

    interim changes

--- src/components/WizardTwo/WizardTwo.js
+++ src/components/WizardTwo/WizardTwo.js
@@ -1,5 +1,7 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {updateCity } from '../../ducks/reducer';
 
 class WizardTwo extends Component {
     render(){
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,67 @@
-function reducer(){
 
+
+// CONTSTANTS
+const initialState = {
+    loanType: 'Home Purchase',
+propertyType: 'Single Family Home',
+city: '',
+propToBeUsedOn: '',
+found: "false",
+realEstateAgent: "false",
+cost: 0,
+downPayment: 0,
+credit: '',
+history: '',
+addressOne: '',
+addressTwo: '',
+addressThree: '',
+firstName: '',
+lastName: '',
+email: ''
 }
+
+// ACTION TYPES DE-TYPO'D
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
+const UPDATE_PROPERTY_TYPE="UPDATE_PROPERTY_TYPE";
+const UPDATE_CITY = 'UPDATE_CITY';
+
+// REDUCER
+function reducer( state = initialState, action ) { 
+    switch( action.type ) {
+      case  UPDATE_LOAN_TYPE:
+        return Object.assign( {}, state, { loanType: action.payload } );
+  
+      case UPDATE_PROPERTY_TYPE:
+        return Object.assign( {}, state, { propertyType: action.payload } );
+        
+      case UPDATE_CITY:
+        return Object.assign( {}, state, { city: action.payload } );
+  
+      default: return state;
+    }
+  }
+
+// EXPORT ACTION CREATORS (so componenets can use them
+)
+export function updateLoanType( loanType ){
+    return {
+      type: UPDATE_LOAN_TYPE,
+      payload: loanType
+    }
+  }
+  
+  export function updatePropertyType( property ) {
+    return {
+      type: UPDATE_PROPERTY_TYPE,
+      payload: property
+    }
+  }
+
+  export function updateCity( city ) {
+    return {
+      type: UPDATE_CITY,
+      payload: city
+    }
+  }
 
 export default reducer;
