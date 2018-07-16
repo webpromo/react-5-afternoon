907642b9ef8fc1b5fd29c044ac1dbbbf5b10709e
Jesse <rank_booster@yahoo.com>
2018-07-16 11:58:21 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </Provider>, document.getElementById('root'));
+
 
---
+++ src/store.js
@@ -0,0 +1,6 @@
+
+import {createStore} from 'redux';
+import reducer from './ducks/reducer';
+
+export default createStore(reducer);
+
